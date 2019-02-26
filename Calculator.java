f90b777efa18dba85c66834bbbe48c9efa088031
uniquear <uniqueratliff@gmail.com>
2019-02-25 18:00:44 -0700

    initial version

---
+++ Calculator.java
@@ -0,0 +1,68 @@
+package cse360assign2;
+
+
+/**Name: Unique Ratliff
+ * ClassID: 532
+ * Assignment #: 2
+ * Description: This program is a basic calculator.
+*/
+
+public class Calculator {
+
+	private int total;
+	
+	/** 
+	 * The class constructor.
+	*/
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** 
+	 * 
+	 * @return Integer that is the calculator's total
+	*/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** 
+	 * Adds a value to the total value.
+	 * @param Integer to be added to the total value 
+	*/
+	public void add (int value) {
+		
+	}
+	
+	/** 
+	 * Subtracts a value from the total value.
+	 * @param Integer to be subtracted from the total value
+	*/
+	public void subtract (int value) {
+		
+	}
+	
+	/** 
+	 * Multiplies a value with the total value.
+	 * @param Integer to multiply the total value by
+	*/
+	public void multiply (int value) {
+		
+	}
+	
+	/** 
+	 * Divides the total by a value .
+	 * @param Integer to divide the total value by
+	*/
+	public void divide (int value) {
+		
+	}
+	
+	/** 
+	 * Displays a history of the operation that have been done so far.
+	 * @return String with the program's history 
+	*/
+	public String getHistory () {
+		return "";
+	}
+}
